9ccf3dc88d94b2728c02dfbd5f92c2f99005849e
koki-ota <koki.ota@woven-planet.global>
2024-02-29 20:12:56 +0900

    Add 01 03 challenge

--- 01_objects/03_challenge/bank_account.cpp
+++ 01_objects/03_challenge/bank_account.cpp
@@ -1,1 +1,26 @@
 // コードを入力してください
+
+#include <iostream>
+#include "./bank_account.h"
+
+BankAccount::BankAccount(double balance) {
+    this->balance = balance;
+}
+
+void BankAccount::deposit(double cash) {
+    if (cash < 0) return;
+    balance += cash;
+    std::cout << "Balance: $" << get_balance() << std::endl;
+};
+
+void BankAccount::withdraw(double cash) {
+    if (cash < 0) return;
+    if (balance > cash) {
+        balance -= cash;
+    } else {
+        std::cout << "Insufficient balance!" << std::endl;
+    }
+    std::cout << "Balance: $" << get_balance() << std::endl;
+};
+
+double BankAccount::get_balance() const { return balance; };
--- 01_objects/03_challenge/bank_account.h
+++ 01_objects/03_challenge/bank_account.h
@@ -1,1 +1,13 @@
 // コードを入力してください
+
+class BankAccount {
+    public:
+        BankAccount(double balance);
+
+        void deposit(double cash);
+        void withdraw(double cash);
+        double get_balance() const;
+
+    private:
+        double balance;
+};
