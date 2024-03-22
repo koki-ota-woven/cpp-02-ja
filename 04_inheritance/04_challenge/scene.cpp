eea363b62d3b22f6540dc56a3186c2f581673b6f
koki-ota <koki.ota@woven-planet.global>
2024-03-22 13:52:41 +0900

    04 04 live session

--- 04_inheritance/04_challenge/scene.cpp
+++ 04_inheritance/04_challenge/scene.cpp
@@ -1,1 +1,15 @@
 // コードを入力してください
+#include "scene.hpp"
+
+void Scene::render() const {
+    for (Drawable* drawable : drawables) {
+        if (drawable == nullptr) {
+            continue;
+        }
+        drawable->draw();
+    }
+}
+
+void Scene::add(Drawable* drawable) {
+    this->drawables.push_back(drawable);
+}
