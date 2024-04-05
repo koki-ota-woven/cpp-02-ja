18740db91c58a26cd9a645319406767697d87054
koki-ota <koki.ota@woven-planet.global>
2024-04-05 15:06:14 +0900

    Challenge 03 02

--- 03_containers_and_templates/02_challenge/stack.hpp
+++ 03_containers_and_templates/02_challenge/stack.hpp
@@ -1,1 +1,34 @@
-// コードを入力してください
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stdexcept>
+
+
+template<typename T, typename Container = std::vector<T>>
+class Stack {
+public:
+    bool is_empty() const {
+        return container.empty();
+    }
+
+    void push(const T& value) {
+        container.push_back(value);
+    }
+
+    void pop() {
+        if (is_empty()) {
+            throw std::out_of_range("Stack is empty");
+        }
+        container.pop_back();
+    }
+
+    T top() const {
+        if (is_empty()) {
+            throw std::out_of_range("Stack is empty");
+        }
+        return container.back();
+    }
+
+private:
+    Container container;
+};
