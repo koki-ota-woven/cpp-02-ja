b3b64f7be4c19b0713a3faf91c4abcf7b0d8605d
koki-ota <koki.ota@woven-planet.global>
2024-04-05 14:58:27 +0900

    Add 03_01 challenge

--- 03_containers_and_templates/01_challenge/print.hpp
+++ 03_containers_and_templates/01_challenge/print.hpp
@@ -1,1 +1,27 @@
-// コードを入力してください
+#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+
+
+template<typename T>
+void print_elements(const T& container) {
+    for (const auto& element : container) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+    std::cout << "std::vector: ";
+    print_elements(vec);
+
+    std::list<int> list = {6, 7, 8, 9, 10};
+    std::cout << "std::list: ";
+    print_elements(list);
+
+    std::deque<int> deque = {11, 12, 13, 14, 15};
+    std::cout << "std::deque: ";
+    print_elements(deque);
+}
